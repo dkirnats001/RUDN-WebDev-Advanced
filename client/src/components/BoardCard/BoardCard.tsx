8e064ccef0bd29f200f012b20fb1bb51f2f46bc1
dkirnat001 <dkirnats@gmail.com>
2026-09-12 11:58:04 +0300

    task 1

--- client/src/components/BoardCard/BoardCard.tsx
+++ client/src/components/BoardCard/BoardCard.tsx
@@ -2,8 +2,10 @@ import styles from "./BoardCard.module.css";
 
 type BoardCardProps = {
   title: string;
+  isDone?: boolean;
 };
 
-export function BoardCard({ title }: BoardCardProps) {
-  return <div className={styles.card}>{title}</div>;
+export function BoardCard({ title, isDone }: BoardCardProps) {
+  console.log({isDone})
+  return <div className={isDone ? styles.isDone : styles.card}>{title}</div>;
 }
